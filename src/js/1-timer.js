6edf736aa183c983e3f53c73f43b8df75e5c553f
Viktor Skalei <skaleiviktor@gmail.com>
2025-02-15 00:06:46 -0500

    hw10_finish_timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,98 @@
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+let userSelectedDate = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("[data-start]");
+let daysEl = document.querySelector("[data-days]");
+let hoursEl = document.querySelector("[data-hours]");
+let minutesEl = document.querySelector("[data-minutes]");
+let secondsEl = document.querySelector("[data-seconds]");
+
+
+const timer = {
+  initTime: null,
+  intervalId: null,
+
+  start() {
+    this.initTime = new Date(userSelectedDate.value);
+    startBtn.classList.remove('active');
+    startBtn.disabled = true;
+    userSelectedDate.disabled = true;
+    
+    
+    
+    this.intervalId = setInterval(() => {
+      this.tick();
+    }, 1000);
+  },
+
+  tick() {
+    const diff = this.initTime - new Date();
+    
+    if (diff <= 0) {
+      clearInterval(this.intervalId);
+
+      startBtn.disabled = false;
+      userSelectedDate.disabled = false;
+      
+      return;
+    }
+
+    
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
+  }
+};
+
+startBtn.addEventListener('click', () => {  
+  timer.start();
+  
+});
+
+flatpickr(userSelectedDate, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates.length > 0) {
+      let selectedDate = selectedDates[0];
+      if (selectedDate.getTime() < new Date().getTime()) {
+        iziToast.error({ message: 'Please choose a date in the future' });
+        startBtn.classList.remove('active');
+      }  else {
+        startBtn.classList.add('active');
+      }
+    }
+  },
+});
+
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+      return String(value).padStart(2, '0');
+};
